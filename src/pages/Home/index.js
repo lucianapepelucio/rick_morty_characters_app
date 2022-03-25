100448303fd3ab2eb5ececa752cd380c8c186b71
Luciana <luciana.lucio@g.globo>
2022-03-25 17:20:55 -0300

    feat: create react-app, Header component and page Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import './home.css';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  const [personagens, setPersonagens] = useState([]);
+
+  useEffect(() => {
+
+    async function loadPersonagens(){
+      const response = await api.get('api/character')
+      //console.log(response.data.results);
+      setPersonagens(response.data.results);
+    }
+
+    loadPersonagens();
+
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="lista-personagens">
+        {personagens.map((personagem) => {
+          return(
+            <article key={personagem.id}>
+              <strong> {personagem.name}</strong>
+              <img src={personagem.image} alt={personagem.name} />
+              <Link to="/">Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
