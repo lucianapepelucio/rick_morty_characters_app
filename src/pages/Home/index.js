6c87463f7ad712f336bb9911329e95390ab25b86
Luciana <luciana.lucio@g.globo>
2022-03-29 14:39:06 -0300

    feat: create handleInputChange and handleSubmit functions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { BsSearch } from 'react-icons/bs'; 
 import './home.css';
 import api from '../../services/api';
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   const [personagens, setPersonagens] = useState([]);
+  const [newPersonagem, setNewPersonagem] = useState('');
 
   useEffect(() => {
 
@@ -18,13 +19,42 @@ export default function Home() {
 
   }, []);
 
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+
+    async function submit(){
+      const response = await api.get(`api/character/${newPersonagem}`);
+
+      console.log(response.data.results); // não retorna nada, verificar
+
+      if(newPersonagem !== response.data.results){
+        alert('Personagem não encontrado, digite o nome correto, por favor!');
+        setNewPersonagem('');
+      }
+
+      setNewPersonagem('');
+    }
+
+    submit();
+
+  }, [newPersonagem]);
+
+  function handleInputChange(e){
+    setNewPersonagem(e.target.value);
+  }
+
   return (
     <div className="container">
 
-      <form className= "formulario" onSubmit={()=>{}}>
-        <input type="text" placeholder="Digite o nome do personagem" />
+      <form className= "formulario" onSubmit={handleSubmit}> 
+        <input 
+        type="text" 
+        placeholder="Digite o nome do personagem" 
+        value={newPersonagem}
+        onChange={handleInputChange}
+        />
 
-        <button className="botao">
+        <button className="botao" type="submit">
           <BsSearch color="black" size={14}/>
         </button>
       </form>
