885709e5878e645f339d92f7b5d230b3db950dc7
Luciana <luciana.lucio@g.globo>
2022-04-06 16:47:45 -0300

    feat: create link at character images

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - [x] Criar uma aplicação React 
 - [x] Listar na tela as fotos e nome dos personagens ao carregar a página (com a API https://rickandmortyapi.com/api/character) 
-- [ ] Adicionar um input e fazer a busca pelo name 
+- [x] Adicionar um input e fazer a busca pelo name 
 - [x] Adicionar rota para as páginas /characters e rota /character/:id 
 - [ ] Adicionar paginação 
 - [ ] Criar um componente Preview quando clicar no thumb do character, carregar as informações
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import './styles.css';
 
@@ -8,71 +8,27 @@ export default function Home() {
   const [pageNumber, setPageNumber] = useState('');
   const [item, setItem] = useState([]);
 
-  //const [personagens, setPersonagens] = useState([]);
-  //const [loading, setLoading] = useState(false);
-  
   useEffect(() => {
-    if (nameCharacter) {
-      setItem([]);
-      console.log(nameCharacter);
-      api.get(`/api/character/?page=${pageNumber}&name=${nameCharacter}`)
-        .then((response) => {
-          setItem(response.data);
-          console.log(response.data);
-        })
-    }
+    setItem([]);
+    console.log(nameCharacter);
+    api.get(`/api/character/?page=${pageNumber}&name=${nameCharacter}`)
+      .then((response) => {
+        setItem(response.data);
+        console.log(response.data);
+
+        // if(response.data.length === 0){
+        //   console.log('Não Tem coisa!');
+        // }
+      })
   }, [pageNumber, nameCharacter]);
 
   useEffect(() => {
     setPageNumber('');
   }, []);
 
-  // useEffect(() => {
-
-  //   async function loadPersonagens(){
-  //     const response = await api.get('api/character')
-  //     setPersonagens(response.data.results);
-  //   }
-
-  //   loadPersonagens();
-
-  // }, []);
-
-  // const handleSubmit = useCallback((e) => {
-  //   e.preventDefault();
-
-  //   async function submit(){
-  //     setLoading(true);
-
-  //     try{
-
-  //       if(displayValue === ''){
-  //         throw new Error('Você precisa digitar o nome do personagem!');
-  //       }
-
-  //       const response = await api.get(`api/character/?page=${pageNumber}&name=${displayValue}`);
-
-  //       console.log(response.data.results);
-
-  //       setDisplayValue('');
-  //       }
-  //       catch(error){
-  //         alert('Personagem não encontrado, digite o nome correto, por favor!');
-  //         setDisplayValue('');
-  //         console.log(error);
-  //       }
-  //       finally{
-  //         setLoading(false);
-  //         setDisplayValue('');
-  //       }
-  //   }
-
-  //   submit();
-
-  // }, [displayValue, pageNumber]);
-
   function handleChange(e){
     setNameCharacter(e.target.value);
+    e.preventDefault();
   }
 
   return (
@@ -84,18 +40,19 @@ export default function Home() {
           placeholder="Digite o nome do personagem" 
           value={nameCharacter} 
           onChange={handleChange} 
-          //loading={loading}
         />
       </div>
       <div className="characters-list">
         {nameCharacter && !item.results && (
-            <span>Carregando...</span>
-          )}
+          <span>Carregando...</span>
+        )}
         {item.results && (
           <ul>
             {item.results.map((character) => (
               <li key={character.id}>
-                <img src={character.image} alt={character.name}/>
+                <Link to={`/character/${character.id}`}>
+                  <img src={character.image} alt={character.name}/>
+                </Link>
                 <strong>{character.name}</strong>
               </li>
             ))}
@@ -103,9 +60,10 @@ export default function Home() {
         )}
       </div>
     </div>
-  );
+  )
 }
 
-//<Link to={`/character/${personagem.id}`}>Acessar</Link>
+
+
 
   
